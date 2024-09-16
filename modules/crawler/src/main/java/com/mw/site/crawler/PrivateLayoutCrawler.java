fdbc5b65aac82e3286971923f3c21514006a6b52
Michael Wall <Michael Wall@DESKTOP-FPKKMS7>
2024-09-16 11:48:11 +0100

    initial checkin

---
+++ modules/crawler/src/main/java/com/mw/site/crawler/PrivateLayoutCrawler.java
@@ -0,0 +1,144 @@
+package com.mw.site.crawler;
+
+import com.liferay.petra.string.StringPool;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.model.Layout;
+import com.liferay.portal.kernel.util.CookieKeys;
+import com.liferay.portal.kernel.util.LocaleUtil;
+
+import java.util.Locale;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.HttpStatus;
+import org.apache.http.StatusLine;
+import org.apache.http.client.CookieStore;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.protocol.HttpClientContext;
+import org.apache.http.impl.client.BasicCookieStore;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.impl.cookie.BasicClientCookie;
+import org.apache.http.util.EntityUtils;
+
+public class PrivateLayoutCrawler {
+
+    public PrivateLayoutCrawler(
+            String cookieHostName, String layoutUrlPrefix, String idEnc, String passwordEnc) {
+
+        HttpClientBuilder httpClientBuilder = HttpClientBuilder.create();
+
+        _httpClient = httpClientBuilder.setUserAgent(_USER_AGENT).build();
+        
+        _cookieHostName = cookieHostName;
+        _layoutUrlPrefix = layoutUrlPrefix;
+
+        initializeCredentials(idEnc, passwordEnc);
+    }
+
+    public String getLayoutContent(Layout layout, Locale locale) {
+
+        try {
+            HttpClientContext httpClientContext =
+                    _getBasicHttpClientContext(_cookieHostName);
+
+            BasicClientCookie guestLanguageIdClientCookie = _createClientCookie(
+                    CookieKeys.GUEST_LANGUAGE_ID, LocaleUtil.toLanguageId(locale),
+                    _cookieHostName);
+
+            httpClientContext.getCookieStore().addCookie(
+                    guestLanguageIdClientCookie);
+
+            String layoutFullURL = _layoutUrlPrefix + layout.getFriendlyURL(locale);
+
+            HttpGet httpGet =
+                    new HttpGet(layoutFullURL);
+
+            HttpResponse httpResponse =
+                    _httpClient.execute(httpGet, httpClientContext);
+
+            StatusLine statusLine = httpResponse.getStatusLine();
+
+            if (statusLine.getStatusCode() == HttpStatus.SC_OK) {
+                return EntityUtils.toString(httpResponse.getEntity());
+            }
+            else {
+                if (_log.isWarnEnabled()) {
+                    _log.warn("HttpStatus " + statusLine.getStatusCode());
+                }
+            }
+        }
+        catch (Exception exception) {
+            if (_log.isWarnEnabled()) {
+                _log.warn("Unable to crawl layout content for layout: " + layout.getFriendlyURL(locale), exception);
+            }
+        }
+
+        return StringPool.BLANK;
+    }
+
+    private HttpClientContext _getBasicHttpClientContext(String hostName) {
+
+        if (_httpClientContext != null) {
+            return _httpClientContext;
+        }
+
+        CookieStore cookieStore = new BasicCookieStore();
+
+        BasicClientCookie autoIdClientCookie =
+                _createClientCookie(CookieKeys.ID, _autoUserIdEnc, hostName);
+        BasicClientCookie autoPasswordClientCookie =
+                _createClientCookie(CookieKeys.PASSWORD, _autoPasswordEnc, hostName);
+        BasicClientCookie rememberMeClientCookie =
+                _createClientCookie(CookieKeys.REMEMBER_ME, _rememberMe, hostName);
+
+        cookieStore.addCookie(autoIdClientCookie);
+        cookieStore.addCookie(autoPasswordClientCookie);
+        cookieStore.addCookie(rememberMeClientCookie);
+
+        HttpClientContext httpClientContext = new HttpClientContext();
+
+        httpClientContext.setCookieStore(cookieStore);
+
+        _httpClientContext = httpClientContext;
+
+        return httpClientContext;
+    }
+
+    private BasicClientCookie _createClientCookie(
+            String cookieName, String cookieValue, String domain) {
+
+        BasicClientCookie basicClientCookie =
+                new BasicClientCookie(cookieName, cookieValue);
+
+        basicClientCookie.setDomain(domain);
+
+        return basicClientCookie;
+    }
+
+    protected void initializeCredentials(String idEnc, String passwordEnc) {
+
+        _autoUserIdEnc = idEnc;
+        _autoPasswordEnc = passwordEnc;
+    }
+
+    public static final String COOKIE_KEYS_CRAWLER_HASH =
+            "COOKIE_KEYS_CRAWLER_HASH";
+
+    private static final Log _log =
+            LogFactoryUtil.getLog(PrivateLayoutCrawler.class);
+
+    private static final String _rememberMe = Boolean.toString(true);
+
+    private static final String _USER_AGENT = "Liferay Page Crawler";
+
+    private String _cookieHostName;
+    private String _layoutUrlPrefix;
+
+    private String _autoPasswordEnc;
+    private String _autoUserIdEnc;
+
+    private HttpClient _httpClient;
+
+    private HttpClientContext _httpClientContext;
+}
---
+++ modules/crawler/src/main/java/com/mw/site/crawler/SiteCrawler.java
@@ -0,0 +1,135 @@
+package com.mw.site.crawler;
+
+
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.model.Company;
+import com.liferay.portal.kernel.model.Group;
+import com.liferay.portal.kernel.model.Layout;
+import com.liferay.portal.kernel.model.User;
+import com.liferay.portal.kernel.service.CompanyLocalService;
+import com.liferay.portal.kernel.service.GroupLocalService;
+import com.liferay.portal.kernel.service.LayoutLocalService;
+import com.liferay.portal.kernel.service.UserLocalService;
+import com.liferay.portal.kernel.util.Portal;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.List;
+import java.util.Locale;
+import java.util.Map;
+
+import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Modified;
+import org.osgi.service.component.annotations.Reference;
+
+@Component(
+		immediate = true,
+		property = {"osgi.command.function=crawlPages", "osgi.command.scope=siteCrawler"},
+		service = SiteCrawler.class
+	)
+public class SiteCrawler {
+
+	@Activate
+	@Modified
+	protected void activate(Map<String, Object> properties) {
+		if (_log.isInfoEnabled()) _log.info("activating");
+	}
+
+	public void crawlPages(String companyIdString, String siteIdString, String cookieHostName, String layoutUrlPrefix, String emailAddress, String emailAddressEnc, String passwordEnc) {
+		
+		long companyId = Long.valueOf(companyIdString);
+		long siteId = Long.valueOf(siteIdString);
+		
+		_log.info("CompanyId: " + companyId);
+		_log.info("SiteId: " + siteId);
+		_log.info("Cookie HostName: " + cookieHostName);
+		_log.info("Layout URL Prefix: " + layoutUrlPrefix);
+		_log.info("EmailAddress: " + emailAddress);
+		_log.info("EmailAddressEnc: " + emailAddressEnc);
+		_log.info("passwordEnc: " + passwordEnc);
+		
+		Company company = companyLocalService.fetchCompany(companyId);
+		
+		if (company == null) {
+			_log.info("Company not found for companyId: " + companyId);
+			System.out.println("Company not found for companyId: " + companyId);
+			
+			return;
+		}
+		
+		User user = userLocalService.fetchUserByEmailAddress(companyId, emailAddress);
+		
+		if (user == null) {
+			_log.info("User not found for emailAddress: " + emailAddress);
+			System.out.println("User not found for emailAddress: " + emailAddress);
+			
+			return;
+		}
+		
+		Group group = groupLocalService.fetchGroup(siteId);
+		
+		if (group == null || !group.isSite()) {
+			_log.info("Site not found for siteId: " + siteId);
+			System.out.println("Site not found for siteId: " + siteId);
+			
+			return;
+		}
+		
+		List<Layout> layouts = layoutLocalService.getLayouts(siteId, true);
+		
+		_log.info("Private Page Count: " + layouts.size());
+		
+		PrivateLayoutCrawler layoutCrawler = new PrivateLayoutCrawler(cookieHostName, layoutUrlPrefix, emailAddressEnc, passwordEnc);
+		
+		String folderName = "/siteExport_" + System.currentTimeMillis();
+		
+		File outputFolder = new File(folderName);
+		if (!outputFolder.exists()) outputFolder.mkdirs();
+		
+		_log.info(outputFolder.getAbsolutePath());
+		
+		for (Layout layout: layouts) {
+			String layoutContent = crawlPageContent(layout, layoutCrawler, user.getLocale());
+			
+			PrintWriter printWriter = null;
+
+			try {
+				printWriter = new PrintWriter(folderName + "/" + layout.getFriendlyURL().replaceAll("/", "_") + ".html");
+				
+				printWriter.println(layoutContent);
+			} catch (FileNotFoundException e) {
+				e.printStackTrace();
+			} finally {
+				if (printWriter != null) printWriter.close();
+				
+				printWriter.close();
+			}
+		}
+	}
+	
+    private String crawlPageContent(
+            Layout layout, PrivateLayoutCrawler layoutCrawler, Locale locale) {
+
+        return layoutCrawler.getLayoutContent(layout, locale);
+    }
+	
+	@Reference
+	private LayoutLocalService layoutLocalService;
+	
+	@Reference
+	private CompanyLocalService companyLocalService;
+	
+	@Reference
+	private GroupLocalService groupLocalService;
+	
+	@Reference
+	private UserLocalService userLocalService;
+	
+    @Reference
+    private Portal _portal;
+	
+	private static final Log _log = LogFactoryUtil.getLog(SiteCrawler.class);	
+}
